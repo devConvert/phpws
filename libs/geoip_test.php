07406b599ae85a01ec703403cdacb30e2b81d2d6
G B <giladbbn2@gmail.com>
2018-02-06 13:03:10 +0200

    phpws

---
+++ libs/geoip_test.php
@@ -0,0 +1,12 @@
+<?php
+
+include "geoip.php";
+
+$ip = !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
+$ip_arr = explode(",", $ip);
+$ip = $ip_arr[0];
+
+$geoip = geoip_open("geoip.dat", GEOIP_MEMORY_CACHE);
+$country_code = geoip_country_code_by_addr($geoip, $ip);
+
+echo "Your IP is " . $ip . "<br>And you country code is " . $country_code;
