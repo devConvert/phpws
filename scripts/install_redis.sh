2b3349ad42a018e450e7765f17065cc175f1d36e
giladbbn2 <giladbbn2@gmail.com>
2018-02-20 19:28:03 +0200

    Update install_redis.sh

--- scripts/install_redis.sh
+++ scripts/install_redis.sh
@@ -1,7 +1,7 @@
 sudo service nginx stop
 sudo service php-fpm stop
-sudo yum install -y epel-release redis
-sudo pecl install igbinary igbinary-devel redis php70w-pear
+sudo yum install -y epel-release redis php70w-pear
+sudo pecl install igbinary igbinary-devel redis 
 sudo bash -c 'echo -e "\nextension=igbinary.so" >> /etc/php.ini'
 sudo bash -c 'echo -e "\nextension=redis.so" >> /etc/php.ini'
 sudo service php-fpm start
