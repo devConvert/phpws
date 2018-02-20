0fc2f6b23a41baa6e13879d2651299598e92ea3f
giladbbn2 <giladbbn2@gmail.com>
2018-02-20 19:18:00 +0200

    Create install_redis.sh

---
+++ scripts/install_redis.sh
@@ -0,0 +1,8 @@
+sudo service nginx stop
+sudo service php-fpm stop
+sudo yum install -y epel-release redis
+sudo pecl install igbinary igbinary-devel redis
+sudo bash -c 'echo -e "\nextension=igbinary.so" >> /etc/php.ini'
+sudo bash -c 'echo -e "\nextension=redis.so" >> /etc/php.ini'
+sudo service php-fpm start
+sudo service nginx start
