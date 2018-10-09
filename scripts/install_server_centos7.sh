edefab2a08cc369ff0ee1d3dcb4af7198afe6248
giladbbn2 <giladbbn2@gmail.com>
2018-10-09 15:28:45 +0300

    Update install_server_centos7.sh

--- scripts/install_server_centos7.sh
+++ scripts/install_server_centos7.sh
@@ -2,7 +2,7 @@
 
 
 # after a fresh install of a new virtual machine checkout the phpws project into /var/www/html
-# and then run /var/www/html/scripts/install_server.sh
+# and then run /var/www/html/scripts/install_server_centos7.sh
 
 # for centos 7.x
 
