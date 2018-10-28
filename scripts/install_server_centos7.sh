999fdf0f96c862ce902d37ed9360729cab70664a
giladbbn2 <giladbbn2@gmail.com>
2018-10-28 14:53:13 +0200

    Update install_server_centos7.sh

--- scripts/install_server_centos7.sh
+++ scripts/install_server_centos7.sh
@@ -11,7 +11,7 @@ sudo yum update -y
 sudo yum install -y epel-release
 
 # on amazone ec2
-sudo amazon-linux-extras install epel
+sudo amazon-linux-extras install epel -y
 
 # update latest yum
 sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
