628a8d9e4f7052287df9a7ca68ff4907e545cd87
giladbbn2 <giladbbn2@gmail.com>
2018-11-21 15:00:24 +0200

    Update install_mysql57.sh

--- scripts/install_mysql57.sh
+++ scripts/install_mysql57.sh
@@ -11,7 +11,7 @@
 sudo rpm -ivh https://dev.mysql.com/get/mysql57-community-release-el6-11.noarch.rpm
 
 # centos7:
-sudo rpm -ivh https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+sudo rpm -ivh https://dev.mysql.com/get/mysql57-community-release-el7-10.noarch.rpm
 
 sudo yum install -y mysql-community-server
 sudo mysqld --initialize
